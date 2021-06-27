51bd7e89d954e36fd516b40644b4bff57fabc785
ryqus27 <ryqus27@gmail.com>
2021-06-27 13:45:44 +0200

    Refaktoryzacja klasy ItemRepository na Interface cz. 2

--- src/main/java/pl/sdacademy/pewex/order/db/ItemRepository.java
+++ src/main/java/pl/sdacademy/pewex/order/db/ItemRepository.java
@@ -3,9 +3,7 @@ package pl.sdacademy.pewex.order.db;
 import org.springframework.data.repository.CrudRepository;
 import pl.sdacademy.pewex.order.model.ItemEntity;
 
-import java.util.Optional;
-
 public interface ItemRepository extends CrudRepository<ItemEntity, Long> {
-    Optional<ItemEntity> findByName(Long name);
+
 
 }
