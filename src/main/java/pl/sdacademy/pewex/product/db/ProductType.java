aa6b5055190a6d1243b1113859ad2ae89d2d477c
Borys <boria81@gmail.com>
2021-06-27 11:56:00 +0100

    Add table for product type as we agreed, enum is not a good way to do it

--- src/main/java/pl/sdacademy/pewex/product/db/ProductType.java
+++ src/main/java/pl/sdacademy/pewex/product/db/ProductType.java
@@ -1,25 +1,21 @@
-
 package pl.sdacademy.pewex.product.db;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
-@Table(name = "category")
-public class CategoryEntity {
-
+public class ProductType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long parentId;
+    private Long Id;
     private String name;
-
 }
