5cc42d9dc4ab0c24d486f3df50f87f390828332e
Anna Ksiazek <aksiazek@softak.eu>
2021-07-03 13:03:55 +0200

    make changes so code can be built

--- src/main/java/pl/sdacademy/pewex/order/model/ItemEntity.java
+++ src/main/java/pl/sdacademy/pewex/order/model/ItemEntity.java
@@ -5,26 +5,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Data
+@Entity
 @Table(name = "item")
 @AllArgsConstructor
 @NoArgsConstructor
 public class ItemEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="id_order")
-    private Long idOrder;
+    @Column(name="order_id")
+    private Long orderId;
 
-    @Column(name="id_product")
-    private Long idProduct;
+    @Column(name="product_id")
+    private Long productId;
 
     private int count;
 
-    private float price;
+    private BigDecimal price;
 
 }
--- src/main/java/pl/sdacademy/pewex/order/model/OrderEntity.java
+++ src/main/java/pl/sdacademy/pewex/order/model/OrderEntity.java
@@ -5,29 +5,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
-
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 
 @Data
-@Table(name = "order")
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "order_t")
 public class OrderEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="id_user")
-    private Long idUser;
+    @Column(name="user_id")
+    private Long userId;
 
-    @Column(name="id_address_delivery")
-    private Long idAddressDelivery;
+    @Column(name="delivery_address_id")
+    private Long deliveryAddressId;
 
     private LocalDateTime date;
 
+    @Enumerated(EnumType.STRING)
     private Status status;
 
 }
