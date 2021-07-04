1e19a161fbfd53b027e9231f2073e7d3ccc3d67e
mikolaj-schonbach <mikolaj.schonbach@gmail.com>
2021-07-04 10:32:01 +0200

    add ProductDetail class (DTO)

---
+++ src/main/java/pl/sdacademy/pewex/product/model/ProductDetail.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.pewex.product.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDetail {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private String description;
+    private Double rating;
+
+
+
+
+}
