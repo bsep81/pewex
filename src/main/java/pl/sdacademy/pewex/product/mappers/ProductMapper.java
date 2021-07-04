9b1c7d8c4ae75f80e88f63416e4996b26bf5f424
macie <maciejczulak@o2.pl>
2021-07-04 11:36:25 +0200

    Add @Component, change mapEntityToProduct into Optional, apply build.

--- src/main/java/pl/sdacademy/pewex/product/mappers/ProductMapper.java
+++ src/main/java/pl/sdacademy/pewex/product/mappers/ProductMapper.java
@@ -1,34 +1,46 @@
 package pl.sdacademy.pewex.product.mappers;
 
+import org.springframework.stereotype.Component;
 import pl.sdacademy.pewex.product.db.ProductEntity;
 import pl.sdacademy.pewex.product.model.Product;
 
+import java.util.Optional;
 
+@Component
 public class ProductMapper {
-    public Product mapEntityToProduct(ProductEntity created) {
-        if (created == null) {
-            return null;
+    public Optional<Product> mapEntityToProduct(ProductEntity entity) {
+
+        if (entity == null) {
+            return Optional.empty();
         }
-        Product result = new Product();
-        result.setId(created.getId());
-        result.setTitle(created.getTitle());
-        result.setAuthor(created.getAuthor());
-        result.setPrice(created.getPrice());
-        result.setDescription(created.getDescription());
-        result.setRating(created.getRating());
-        return result;
+
+        Product product = Product.builder()
+                .id(entity.getId())
+                .title(entity.getTitle())
+                .author(entity.getAuthor())
+                .price(entity.getPrice())
+                .description(entity.getDescription())
+                .rating(entity.getRating())
+                .build();
+
+        return Optional.of(product);
     }
 
     public ProductEntity mapProductToEntity(Product product) {
-        ProductEntity entity = new ProductEntity();
-        entity.setId(product.getId());
-        entity.setTitle(product.getTitle());
-        entity.setAuthor(product.getAuthor());
-        entity.setPrice(product.getPrice());
-        entity.setDescription(product.getDescription());
-        entity.setRating(product.getRating());
-        return entity;
 
+        return ProductEntity.builder()
+                .title(product.getTitle())
+                .author(product.getAuthor())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .rating(product.getRating())
+                .build();
     }
+
+
+
+
+
+
 }
 
