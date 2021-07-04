539cfe17cf8671e35ae3532541b831ac6096fc6b
Borys <boria81@gmail.com>
2021-07-04 12:30:42 +0100

    add ProductServiceInterface

---
+++ src/main/java/pl/sdacademy/pewex/product/service/ProductServiceInterface.java
@@ -0,0 +1,14 @@
+package pl.sdacademy.pewex.product.service;
+
+import pl.sdacademy.pewex.product.model.ProductDetail;
+import pl.sdacademy.pewex.product.model.ProductListDTO;
+
+import java.util.List;
+
+public interface ProductServiceInterface {
+
+    List<ProductListDTO> getProductList();
+
+    ProductDetail getProductDetail(Long id);
+
+}
