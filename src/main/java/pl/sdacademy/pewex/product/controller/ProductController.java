e775d674b1b20903ec5ca565d6e493c044684aad
Borys <boria81@gmail.com>
2021-07-06 22:19:35 +0100

    add basic get productDetails feature

--- src/main/java/pl/sdacademy/pewex/product/controller/ProductController.java
+++ src/main/java/pl/sdacademy/pewex/product/controller/ProductController.java
@@ -1,8 +1,10 @@
 package pl.sdacademy.pewex.product.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sdacademy.pewex.product.model.ProductDetail;
 import pl.sdacademy.pewex.product.model.ProductListDTO;
 import pl.sdacademy.pewex.product.service.ProductService;
 
@@ -18,9 +20,14 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("list")
+    @GetMapping("/list")
     public List<ProductListDTO> getList(){
         return productService.getProductList();
+    }
+
+    @GetMapping("/{id}")
+    public ProductDetail getProductDetail(@PathVariable(name = "id", required = true) Long id){
+        return productService.getProductDetail(id);
     }
 
 }
--- src/main/java/pl/sdacademy/pewex/product/service/ProductService.java
+++ src/main/java/pl/sdacademy/pewex/product/service/ProductService.java
@@ -1,24 +1,29 @@
 package pl.sdacademy.pewex.product.service;
 
 import org.springframework.stereotype.Component;
+import pl.sdacademy.pewex.product.db.ProductEntity;
 import pl.sdacademy.pewex.product.mappers.ProductListDTOMapper;
+import pl.sdacademy.pewex.product.mappers.ProductMapper;
 import pl.sdacademy.pewex.product.model.ProductDetail;
 import pl.sdacademy.pewex.product.model.ProductListDTO;
 import pl.sdacademy.pewex.product.repository.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ProductService implements ProductServiceInterface{
 
     private final ProductRepository productRepository;
     private final ProductListDTOMapper productListDTOMapper;
+    private final ProductMapper productMapper;
 
-    public ProductService(ProductRepository productRepository, ProductListDTOMapper productListDTOMapper) {
+    public ProductService(ProductRepository productRepository, ProductListDTOMapper productListDTOMapper, ProductMapper productMapper) {
         this.productRepository = productRepository;
         this.productListDTOMapper = productListDTOMapper;
 
+        this.productMapper = productMapper;
     }
 
 
@@ -32,7 +37,11 @@ public class ProductService implements ProductServiceInterface{
 
     @Override
     public ProductDetail getProductDetail(Long id) {
-        return null;
+        Optional<ProductEntity> productEntityOptional = productRepository.findById(id);
+        if(productEntityOptional.isEmpty()){
+            return new ProductDetail();
+        }
+        return productMapper.mapEntityToProductDetail(productEntityOptional.get()).get();
     }
 
 
