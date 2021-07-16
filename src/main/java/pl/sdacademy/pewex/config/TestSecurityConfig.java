e01a4229cc61cdaa5eacbc9c8331833463e5864d
Borys <boria81@gmail.com>
2021-07-16 23:29:39 +0100

    add configuration for testing REST endpoints

---
+++ src/main/java/pl/sdacademy/pewex/config/TestSecurityConfig.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.pewex.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile("test")
+public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
